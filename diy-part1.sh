5a1309b2e381aa7cc36c83aba6eefab7a0edbd41
77-QiQi <87052253+77-QiQi@users.noreply.github.com>
2026-06-06 13:56:53 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,5 +18,7 @@
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo 'src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main' >> feeds.conf.default
 echo 'src-git passwall_luci https://github.com/Openwrt-Passwall/openwrt-passwall.git;main' >> feeds.conf.default
+echo 'src-git openwrt_packages https://github.com/77-QiQi/packages.git;master' >> feeds.conf.default
+echo 'src-git openwrt_luci https://github.com/77-QiQi/luci.git;master' >> feeds.conf.default
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small-package' >> feeds.conf.default
